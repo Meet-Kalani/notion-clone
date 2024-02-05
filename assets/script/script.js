3a89a1e970826cec988716bbb93d60dba0ee88a7
Meet Kalani <meetkalani2002@gmail.com>
2024-02-05 17:46:45 +0530

    add comments

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,6 +2,7 @@ let navbars = document.querySelectorAll(".navbar");
 let mobileNavbarBtn = document.querySelector(".mobile-navbar .free-notion-btn")
 let languageContainer = document.querySelector('.language-container')
 
+// navbar toggle for small devices
 function openNavbar(){
     navbars.forEach(function(navbar) {
         if (navbar.style.display === "none" || navbar.style.display === "") {
@@ -14,14 +15,7 @@ function openNavbar(){
     });
 }
 
-function toggleLanguage(){
-    if(languageContainer.style.display === "block"){
-        languageContainer.style.display = "none";
-    } else {
-        languageContainer.style.display = "block";
-    }
-}
-
+// display navbar after exceeding 1080px even if it is closed in smaller devices
 window.addEventListener('resize', () => {
     navbars.forEach(function(navbar) {
         if (window.innerWidth > 1080) {
@@ -33,6 +27,16 @@ window.addEventListener('resize', () => {
     });
 });
 
+// language toggle
+function toggleLanguage(){
+    if(languageContainer.style.display === "block"){
+        languageContainer.style.display = "none";
+    } else {
+        languageContainer.style.display = "block";
+    }
+}
+
+// function for closing the language container when user clicks on any element which is not language container
 window.addEventListener('click', function(event) {
     if (!languageContainer.contains(event.target) && event.target.tagName !== 'BUTTON') {
       languageContainer.style.display = 'none';
